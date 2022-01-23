ad2a18827dbf2961e3e428ca7ffa4acda61e8f92
leobatouxas <leo.batouxas@gmail.com>
2022-01-23 18:03:28 +0100

    Ajout BackOffice Admin

---
+++ src/Controller/Admin/UtilisateurCrudController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Utilisateur;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class UtilisateurCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Utilisateur::class;
+    }
+
+    
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('email'),
+            ChoiceField::new('roles', 'Roles')
+                    ->allowMultipleChoices()
+                    ->autocomplete()
+                    ->setChoices([  'Utilisateur' => 'ROLE_USER',
+                                    'Employé' => 'ROLE_EMPLOYE',
+                                    'Administrateur' => 'ROLE_ADMIN']
+                                )
+        ];
+    }
+    
+}
